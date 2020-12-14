8392c793cb15082b4dc89f1c704a58d76f2545f4
Natalia-Duzhak <72571464+Natalia-Duzhak@users.noreply.github.com>
2020-12-14 18:09:30 +0300

    Add files via upload

---
+++ UnitTest11.2/UnitTest11.2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project10.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest112
+{
+	TEST_CLASS(UnitTest112)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			bool m;
+			char fname[] = "1.dat";
+				m = Number(fname);
+			Assert::AreEqual(m, false);
+		}
+	};
+}
